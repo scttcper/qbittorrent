02998ae5dd810396572dd02f0a0272153639c1dc
danielsss <better.sunjian@gmail.com>
2024-07-09 02:27:34 +1000

    added: getDefaultSavePath

---
+++ scripts/start_container.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/bash
+
+
+docker run -d \
+  --name=qbittorrent \
+  -e PUID=1000 \
+  -e PGID=1000 \
+  -e TZ=Etc/UTC \
+  -e WEBUI_PORT=8080 \
+  -p 8080:8080 \
+  -p 6881:6881 \
+  -p 6881:6881/udp \
+  --restart unless-stopped \
+  lscr.io/linuxserver/qbittorrent:latest
---
+++ scripts/stop_container.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/bash
+
+
+docker rm -f qbittorrent
