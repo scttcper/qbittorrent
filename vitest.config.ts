0a9d27b8763b5c08989ee034bc00e8487f54769d
Scott Cooper <scttcper@gmail.com>
2025-11-05 22:39:48 -0800

    test: exclude build folder

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
   test: {
+    exclude: ['**/node_modules/**', '**/dist/**', '**/.git/**'],
     coverage: {
       provider: 'v8',
       reporter: ['text', 'json', 'html'],
